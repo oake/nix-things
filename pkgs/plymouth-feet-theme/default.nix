14e25bbffa2e76c7fb947848cfd3df77bd6dedc9
Anna Oake <anna@oa.ke>
2025-07-07 18:39:31 +0200

    plymouth-feet-theme: init

---
+++ pkgs/plymouth-feet-theme/default.nix
@@ -0,0 +1,23 @@
+{
+  stdenvNoCC,
+  fetchgit,
+  lib,
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "plymouth-feet-theme";
+  version = "1.0.0";
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/share/plymouth/themes/feet
+    cp -r ${./feet}/* $out/share/plymouth/themes/feet
+    find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
+  '';
+
+  meta = {
+    description = "Feet Plymouth boot splash";
+    platforms = lib.platforms.linux;
+  };
+}
