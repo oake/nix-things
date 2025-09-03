819832e457a2c1df8abbaed4e95b4274c423af45
Anna Oake <anna@oa.ke>
2025-09-03 23:28:31 +0200

    me: add wifeKey and lanDomain

--- modules/common/me.nix
+++ modules/common/me.nix
@@ -7,5 +7,7 @@
     username = lib.mkOption { type = lib.types.str; };
     email = lib.mkOption { type = lib.types.str; };
     sshKey = lib.mkOption { type = lib.types.str; };
+    wifeKey = lib.mkOption { type = lib.types.str; };
+    lanDomain = lib.mkOption { type = lib.types.str; };
   };
 }
