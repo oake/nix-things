6c12fdb6cd7d7b8c5a12b7c9e9fa1bdd886f2c40
Anna Oake <anna@oa.ke>
2025-09-11 15:09:35 +0200

    remote-builders: init

---
+++ modules/common/remote-builders.nix
@@ -0,0 +1,69 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  availableMachines = {
+    gratis = {
+      hostName = "gratis.oa.ke";
+      system = "aarch64-linux";
+      maxJobs = 4;
+      supportedFeatures = [
+        "benchmark"
+        "big-parallel"
+        "gccarch-armv8-a"
+        "kvm"
+        "nixos-test"
+      ];
+      protocol = "ssh-ng";
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUVqNkVSbk5ldUxuL0ZsTXFxN3pTVFhYbFBDLzFiVTlxT1lsYTNGTUJvbFMgZ3JhdGlzCg==";
+    };
+    "lxc-builder" = {
+      hostName = "builder.lan.ci";
+      system = "x86_64-linux";
+      maxJobs = 10;
+      supportedFeatures = [
+        "benchmark"
+        "big-parallel"
+        "kvm"
+        "nixos-test"
+      ];
+      protocol = "ssh-ng";
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUU5aitSdGpaMDdWeXhDVDduSmk2Y3RUeUFWOGFsdUxaU3dncHptWmdGTkogbHhjLWJ1aWxkZXIK";
+    };
+  };
+in
+{
+  options.remoteBuilders = {
+    auto = lib.mkEnableOption "required remote builders automatically";
+    machines = lib.mapAttrs (name: machine: {
+      enable = lib.mkEnableOption "${machine.system} remote builder (${machine.hostname})" // {
+        default = config.remoteBuilders.auto && config.nixpkgs.hostPlatform.system != machine.system;
+      };
+      sshUser = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = config.me.username;
+        description = "The username to log in as on ${machine.hostname}.";
+      };
+      sshKey = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+        description = "The path to the SSH private key with which to authenticate on ${machine.hostname}.";
+      };
+    }) availableMachines;
+  };
+
+  config = {
+    nix.distributedBuilds = config.nix.buildMachines != [ ];
+
+    nix.buildMachines = lib.mapAttrsToList (
+      name: cfg:
+      availableMachines.${name}
+      // {
+        sshUser = cfg.sshUser;
+        sshKey = cfg.sshKey;
+      }
+    ) (lib.attrsets.filterAttrs (name: machine: machine.enable) config.remoteBuilders.machines);
+  };
+}
