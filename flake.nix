8257d45808ee7f19f8b1ae4a7bd916fe1330ee57
Anna Oake <anna@oa.ke>
2025-11-08 22:42:34 +0100

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     deploy-rs = {
