d11b67f467489c6536a9fac572d051880c8b1832
Anna Oake <anna@oa.ke>
2025-12-22 21:24:58 +0100

    hammerspoon: init at 1.0.0

---
+++ packages/hammerspoon/default.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  pname,
+}:
+let
+  inherit (pkgs)
+    stdenv
+    fetchzip
+    lib
+    ;
+in
+stdenv.mkDerivation rec {
+  inherit pname;
+  version = "1.0.0";
+
+  src = fetchzip {
+    name = "${pname}-${version}-source.zip";
+    url = "https://github.com/Hammerspoon/hammerspoon/releases/download/${version}/Hammerspoon-${version}.zip";
+    sha256 = "sha256-CuTFI9qXHplhWLeHS7bgZJolULbg9jQRyT6MTKzkQqs=";
+    stripRoot = false;
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/Applications
+    cp -r $src/Hammerspoon.app $out/Applications/
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://www.hammerspoon.org";
+    description = "Staggeringly powerful macOS desktop automation with Lua";
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    license = lib.licenses.mit;
+    platforms = [
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  };
+}
