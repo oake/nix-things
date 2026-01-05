1968da718b1a1fd8faca99e25ff290f86d11d426
Anna Oake <anna@oa.ke>
2026-01-05 21:55:52 +0100

    nixos: add monitoring module

---
+++ modules/nixos/monitoring.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.monitoring;
+in
+{
+  options.monitoring = {
+    logs = {
+      enable = lib.mkEnableOption "pushing logs to SIEM";
+      target = lib.mkOption {
+        type = lib.types.str;
+        description = "Hostname or IP address to push syslog messages to";
+      };
+      port = lib.mkOption {
+        type = lib.types.int;
+        default = 3514;
+        description = "Port to push syslog messages to";
+      };
+    };
+    machineType = lib.mkOption {
+      type = lib.types.enum [
+        "local"
+        "remote"
+        "mobile"
+      ];
+      default = "local";
+    };
+  };
+  config = lib.mkIf cfg.logs.enable {
+    services.rsyslogd = {
+      enable = true;
+      extraConfig = ''
+        *.* action(
+          type="omfwd"
+          target="${cfg.logs.target}"
+          port="${toString cfg.logs.port}"
+          protocol="tcp"
+          template="RSYSLOG_SyslogProtocol23Format"
+
+          action.resumeRetryCount="-1"
+
+          queue.type="LinkedList"
+          queue.filename="fwdqueue"
+          queue.maxdiskspace="1g"
+          queue.saveonshutdown="on"
+        )
+      '';
+    };
+  };
+}
