e6ea252dbac766be8a8b6dde24340cf8ab15b953
Anna Oake <anna@oa.ke>
2026-01-12 16:54:30 +0100

    proxmoxbar: 1.0.5 -> 1.0.6

--- packages/proxmoxbar/default.nix
+++ packages/proxmoxbar/default.nix
@@ -14,11 +14,11 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   inherit pname;
-  version = "1.0.5";
+  version = "1.0.6";
 
   src = fetchurl {
     url = "https://github.com/ryzenixx/proxmoxbar-macos/releases/download/v${finalAttrs.version}/ProxmoxBar.dmg";
-    hash = "sha256-+sTFvvrKHu1GxTSN6mhIupudZpR+oEnufKxstUhHt+k=";
+    hash = "sha256-eOynWzAVWKG/lbT1pe1nZdL7cwgiZmm6lEKvM9hwTms=";
   };
   sourceRoot = ".";
 
