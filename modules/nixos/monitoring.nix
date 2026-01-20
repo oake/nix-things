adc42c4d694690640d7e6739bab646a3fcba8e9d
Anna Oake <anna@oa.ke>
2026-01-20 17:52:42 +0100

    monitoring: allow docker daemon configuration

--- modules/nixos/monitoring.nix
+++ modules/nixos/monitoring.nix
@@ -8,18 +8,6 @@ let
 in
 {
   options.monitoring = {
-    logs = {
-      enable = lib.mkEnableOption "pushing logs to SIEM";
-      target = lib.mkOption {
-        type = lib.types.str;
-        description = "Hostname or IP address to push messages to";
-      };
-      port = lib.mkOption {
-        type = lib.types.int;
-        default = 12202;
-        description = "Port of a GELF TCP input to push messages to";
-      };
-    };
     machineType = lib.mkOption {
       type = lib.types.enum [
         "local"
@@ -28,39 +16,78 @@ in
       ];
       default = "local";
     };
-  };
-  config = lib.mkIf cfg.logs.enable {
-    services.fluent-bit = {
-      enable = true;
-      settings = {
-        service.log_level = "warn";
-        pipeline = {
-          inputs = [
-            {
-              name = "systemd";
-              tag = "journal.*";
 
-              read_from_tail = true;
-              lowercase = true;
-              strip_underscores = true;
-            }
-          ];
-          outputs = [
-            {
-              name = "gelf";
-              match = "journal.*";
+    logs = {
+      target = lib.mkOption {
+        type = lib.types.str;
+        description = "Hostname or IP address to push messages to";
+      };
 
-              host = cfg.logs.target;
-              port = cfg.logs.port;
-              mode = "tcp";
+      systemd = {
+        enable = lib.mkEnableOption "pushing systemd logs to SIEM";
+        port = lib.mkOption {
+          type = lib.types.int;
+          default = 12202;
+          description = "Port of a GELF TCP input to push systemd messages to";
+        };
+      };
 
-              gelf_short_message_key = "message";
-              gelf_host_key = "hostname";
-              gelf_level_key = "priority";
-            }
-          ];
+      docker = {
+        enable = lib.mkEnableOption "pushing Docker logs to SIEM";
+        port = lib.mkOption {
+          type = lib.types.int;
+          default = 12201;
+          description = "Port of a GELF TCP input to push Dockermessages to";
         };
       };
     };
   };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.logs.systemd.enable {
+      services.fluent-bit = {
+        enable = true;
+        settings = {
+          service.log_level = "warn";
+          pipeline = {
+            inputs = [
+              {
+                name = "systemd";
+                tag = "journal.*";
+
+                read_from_tail = true;
+                lowercase = true;
+                strip_underscores = true;
+              }
+            ];
+            outputs = [
+              {
+                name = "gelf";
+                match = "journal.*";
+
+                host = cfg.logs.target;
+                port = cfg.logs.systemd.port;
+                mode = "tcp";
+
+                gelf_short_message_key = "message";
+                gelf_host_key = "hostname";
+                gelf_level_key = "priority";
+              }
+            ];
+          };
+        };
+      };
+    })
+    (lib.mkIf cfg.logs.docker.enable {
+      virtualisation.docker.daemon.settings = {
+        log-driver = "gelf";
+        log-opts = {
+          gelf-address = "tcp://${cfg.logs.target}:${toString cfg.logs.docker.port}";
+          tag = "{{.ImageName}}/{{.Name}}/{{.ID}}";
+          labels = "com.docker.compose.project,com.docker.compose.service";
+          mode = "non-blocking";
+          max-buffer-size = "4m";
+        };
+      };
+    })
+  ];
 }
