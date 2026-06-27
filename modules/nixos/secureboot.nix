3161177bc6fc4bd87ff76565c2d0a70a934a2900
Anna Oake <anna@oa.ke>
2026-06-27 20:47:24 +0200

    secureboot: remove deprecated option

--- modules/nixos/secureboot.nix
+++ modules/nixos/secureboot.nix
@@ -19,7 +19,6 @@
     boot.loader.systemd-boot.enable = lib.mkForce false;
     boot.loader.efi.canTouchEfiVariables = true;
 
-    boot.bootspec.enable = true;
     boot.lanzaboote = {
       enable = true;
       autoGenerateKeys.enable = true;
